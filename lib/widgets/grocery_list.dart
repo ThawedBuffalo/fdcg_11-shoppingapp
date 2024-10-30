91305535e0b8a85f290693185357e17488675011
JC Reid <thawedbuffalo@gmail.com>
2024-10-29 21:55:32 -0400

    chapter 11 section 8

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,15 +1,33 @@
+import 'package:fdcg11shoppingapp/widgets/new_item.dart';
 import 'package:flutter/material.dart';
 
 import '../data/dummy_items.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+        MaterialPageRoute(builder: (ctx) => const NewItem(),),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
