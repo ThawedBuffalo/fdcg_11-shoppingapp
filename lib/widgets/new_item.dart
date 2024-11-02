d6c35f8628d965bbbd721df581047e602d7f328c
JC Reid <thawedbuffalo@gmail.com>
2024-11-01 22:05:32 -0400

    chapter 12main section 8

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -2,6 +2,10 @@ import 'package:fdcg11shoppingapp/models/category.dart';
 import 'package:fdcg11shoppingapp/models/grocery_item.dart';
 import 'package:flutter/material.dart';
 import 'package:fdcg11shoppingapp/data/categories.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
 
@@ -15,12 +19,26 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      Navigator.of(context).pop(GroceryItem(
-          id: DateTime.now().toString(), name: _enteredName,
-          quantity: _enteredQuantity, category: _selectedCategory));
+      final url = Uri.https('flutter-bootcamp-ba357-default-rtdb.firebaseio.com', 'shopping-list.json');
+      final response = await http.post(url, headers: {
+        'Content-Type': 'application/json',
+      }, body: json.encode({
+        'name': _enteredName,
+        'quantity': _enteredQuantity,
+        'category': _selectedCategory.title
+      },),);
+
+      print(response.statusCode);
+      print(response.body);
+
+      if (!context.mounted){
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
